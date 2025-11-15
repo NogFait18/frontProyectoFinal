0e5c9444c4097b27946fa46632218e3ee5e2aec4
Leonardo Gomez <lfglfg0804@gmail.com>
2025-11-15 16:04:21 -0300

    resolviendo conflictos en style

--- src/pages/client/orders/orders.ts
+++ src/pages/client/orders/orders.ts
@@ -1,4 +1,54 @@
 import { mostrarPedidosCliente, mostrarPedidos, mostrarPedidosPorEstadoCliente, traerUsuarioPorId } from "../../../utils/api";
+import { obtenerCategorias } from "../../../utils/api";
+import type { ICategoriaMostrar } from "../../../types/ICategoria";
+
+
+
+//Logica del sidebar de categorias
+
+// ------------------- SELECTORES -------------------
+const categoriaPanel = document.getElementById("categoriasPanel") as HTMLElement | null;
+
+
+// ------------------- LOGICA -------------------
+
+const cargarCategorias = async (): Promise<void> => {
+
+    try{
+        const categorias = await obtenerCategorias();
+        if (!categoriaPanel) return;
+        categoriaPanel.innerHTML = `
+        <section class="panelControl">
+        <button class="categoria_btn-filtro" id="btnVerTodo">🍽️ Ver todo</button>
+        </section>`;
+
+        categorias.forEach((cat: ICategoriaMostrar) => {
+            const sectionNuevaCat = document.createElement("section");
+            sectionNuevaCat.classList.add("panelControl");
+
+            sectionNuevaCat.innerHTML= `
+        <button class="categoria_btn-filtro" data-id="${cat.id}">
+        ${cat.nombre}
+        </button>`;
+
+        categoriaPanel.appendChild(sectionNuevaCat);
+        })
+
+
+    } catch (err) {
+        console.error("Se produjo un error al listar las categorias: ", err);
+        
+    }
+};
+
+cargarCategorias();
+
+
+
+
+
+
+
 
 const pedidos = document.getElementById("card_containerPed");
 const filtroSelect = document.getElementById("filtroPedidos") as HTMLSelectElement;
@@ -42,6 +92,12 @@ console.log(pedidosMostrar)
       <p><strong>Total:</strong> $${p.total}</p>
     </div>
   `;
+
+     div.addEventListener("click", () => {
+        abrirModalConPedido(p);  // función que vas a crear
+        }); 
+
+
   pedidos?.appendChild(div);
 }
 //numero de pedido, fecha, estado
@@ -87,5 +143,61 @@ filtroSelect?.addEventListener("change", () => {
   cargarPedidosPorEstado(estado);
 });
 
+
+
+/*   Funcion para abrir el modal del pedido Cliente */
+
+function abrirModalConPedido(pedido: any){
+    const modal = document.getElementById("modalPedido")!;
+    const div = document.getElementById("modalPedidoContenido")!;
+    div.classList = "";
+    div.classList.add(`pedido-${pedido.estado.toLowerCase()}`); 
+
+      div.innerHTML = `
+
+        <div class="pedido-header">
+            <h4>Pedido #${pedido.id}</h4>
+            <span class= "estado" >${pedido.estado}</span>
+        </div>
+    <div class = "modal_body">    
+        <p><strong>Fecha:</strong> ${pedido.fecha}</p>
+        <p><strong>Estado:</strong> ${pedido.estado}</p>
+
+        <h4>Productos</h4>
+        <ul>
+        ${pedido.detalles
+            .map(
+        (d: any) =>
+
+        `   <li class="item-producto">
+            <img src="${d.productoDto.imagen}" alt="${d.productoDto.nombre}">
+            <span>${d.productoDto.nombre}</span>
+            <span>$${d.productoDto.precio}</span>
+            <span>x${d.cantidad}</span>
+            </li>`
+        )
+        .join("")}
+
+        </ul>
+
+            <p class = "detallePedido_total" ><strong>Total:</strong> $${pedido.total}</p>
+        </div>
+        `;
+
+  modal.classList.remove("oculto");
+
+}
+
+
+
+
 // Carga inicial
 cargarPedidos();
+
+
+
+// Evento para cerrar el modal de pedido
+
+document.getElementById("cerrarModal")?.addEventListener("click", () => {
+  document.getElementById("modalPedido")?.classList.add("oculto");
+});
